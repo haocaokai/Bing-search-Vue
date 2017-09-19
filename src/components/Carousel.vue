640e5fd2c0ec14b8989f290dadb139450296b6b1
cakai <307356224@qq.com>
2017-09-19 22:21:45 +0800

    add carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 所有组件写在一个父容器里 -->
+  <div>
+    <h1>{{msg}}</h1>
+    
+  </div>
+  
+</template>
+
+
+<script>
+export default  {
+  name : 'carousel',
+  data(){
+    return {
+      msg : 'carousel',
+      
+    }
+  }
+}
+
+</script>
+
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
 import Tab from '@/components/Tab';
+import Carousel from '@/components/Carousel';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/tab',
       name: 'Tab',
       component: Tab
+    },
+    {
+      path: '/carousel',
+      name: 'Carousel',
+      component: Carousel
     }
   ]
 });
